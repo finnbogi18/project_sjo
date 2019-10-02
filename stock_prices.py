f6e492a57ff2d52b740ec412c5e1c8bfb0fc7ec5
Finnbogi Jakobsson <finnbogi18@ru.is>
2019-10-02 11:46:34 +0000

    eh spæsi

--- stock_prices.py
+++ stock_prices.py
@@ -1,2 +1,23 @@
-''' This program takes a .csv file that the user inputs, then the program reads the file and finds the average price for each month
-    and prints out the average price and the month of that price, finally it prints out the year-month-day of the highest price '''
+''' This program takes a .csv file that the user inputs, then the program reads 
+    the file and finds the average price for each month
+    and prints out the average price and the month of that price, 
+    finally it prints out the year-month-day of the highest price '''
+
+import csv
+
+
+def open_file(filename):
+    try:
+        with open(filename) as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=",")
+            line_count = 0
+            temp_list = []
+            main_data = []
+            for row in csv_reader:
+                temp_list.append(row)
+                main_data.append(temp_list)
+                line_count += 1
+            return main_data, line_count
+
+    except FileNotFoundError:
+        print("Filename {} not found!".format(filename))
